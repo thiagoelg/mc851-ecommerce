7ad8af3a47a8dedfbfe4127bdce41c25d80195b5
Caio Salvador <caiosalvador96@gmail.com>
2018-05-02 02:05:38 -0300

    Add /products request, not complete

--- back/src/server.js
+++ back/src/server.js
@@ -1,16 +1,44 @@
-import express from "express";
-import bodyParser from "body-parser";
-    
+import express from "express"
+import bodyParser from "body-parser"
+import axios from "axios"
+
 var app  = express(),
-    port = process.env.PORT || 3001;
+    port = process.env.PORT || 3001,
+    produtosURL = "https://ftt-catalog.herokuapp.com";
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/ping', (req, res) => {
-    res.send('pong\n');
-});
+    res.send('pong\n')
+})
+
+app.get('/products', (req, res) => {
+
+    let page           = req.query.page           || 1
+    let highlight      = req.query.highlight      || false
+    let brand          = req.query.brand          || ""
+    let category_id    = req.query.category_id    || ""
+    let max_price      = req.query.max_price      || ""
+    let min_price      = req.query.min_price      || ""
+    let name           = req.query.name           || ""
+    let parent_product = req.query.parent_product || ""
+
+    axios.get(produtosURL + "/products", {
+        params: {
+          page: 1 //TODO: {page}
+        }
+    })  .then(response => {
+            res.send(response.data)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+})
 
 app.listen(port,() => {
     console.log("Listening on port " + port)
-});
+})
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -23,6 +51,6 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     if(err.status != 404) {
         console.error(err.stack)
-    } 
+    }
     res.sendStatus(err.status || 500)
 })
