c4437ebdfa3d90f13c401369bb425dcf2b299c9e
Thiago Lugli <thiagoelg@gmail.com>
2018-05-03 23:23:22 -0300

    destaques usando a versao @next do material-ui

--- front/src/components/Body/Body.js
+++ front/src/components/Body/Body.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import Grid from "material-ui/es/Grid/Grid";
 import Typography from "material-ui/es/Typography/Typography";
+import Destaques from "../Destaques/Destaques"
 
 class Body extends Component {
     render() {
         return (
             <Grid container xs={12} style={{marginTop: 75, padding: 20}}>
                 <Grid item xs={12}>
-                    <Typography variant="title" color="inherit">
-                        Destaques
-                    </Typography>
+                    <Destaques/>
                 </Grid>
             </Grid>
         );
---
+++ front/src/components/Destaques/tileData.js
@@ -0,0 +1,44 @@
+const tileData = [
+  {
+    img: './images/geladeira.jpg',
+    title: 'Geladeira',
+    price: 'R$ 4000,00',
+  },
+  {
+    img: './images/guarda.jpeg',
+    title: 'Guarda Roupas',
+    price: 'R$ 1500,00',
+  },
+  {
+    img: './images/iphone.jpeg',
+    title: 'Iphone X',
+    price: 'R$ 7000,00',
+  },
+  {
+    img: './images/maquina.jpeg',
+    title: 'Máquina de Lavar',
+    price: 'R$ 2000,00',
+  },
+  {
+    img: './images/note.jpeg',
+    title: 'Notebook',
+    price: 'R$ 1600,00',
+  },
+  {
+    img: './images/secador.jpeg',
+    title: 'Secador de Cabelo',
+    price: 'R$ 400,00',
+  },
+  {
+    img: './images/sofa.jpeg',
+    title: 'Sofá',
+    price: 'R$ 1800,00',
+  },
+  {
+    img: './images/TV.jpeg',
+    title: 'Televisão',
+    price: 'R$ 2500,00',
+  },
+];
+
+export default tileData;
