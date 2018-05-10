241973a2444506cdb123bb007411f133f261c497
Tamara Martinelli de Campos <tamaramcampos02@gmail.com>
2018-05-09 23:37:33 -0300

    Retirando divs a mais

--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -33,94 +33,79 @@ class Header extends Component {
 
     render() {
         return (
-            <div>
-                <AppBar position="fixed">
-                    <Toolbar>
-                        <Grid container spacing={0} alignItems="center" justify="center">
-                            <Grid item xs={12} sm={3} alignContent="center">
-                                <Logo/>
-                            </Grid>
+            <AppBar position="fixed">
+                <Toolbar>
+                    <Grid container spacing={0} alignItems="center" justify="center">
+                        <Grid item xs={12} sm={3} alignContent="center">
+                            <Logo/>
+                        </Grid>
 
-                            <Grid item xs={12} sm={6} alignContent="center" justify="center">
-                                <SearchInput/>
-                            </Grid>
+                        <Grid item xs={12} sm={6} alignContent="center" justify="center">
+                            <SearchInput/>
+                        </Grid>
 
-                            <Grid item xs={12} sm={3} alignContent="center" justify="center">
-                                <Grid container spacing={8} alignItems="center">
-                                    <Grid item xs={0} sm={3} alignContent="center" justify="center"/>
-                                    <Grid item xs={6} sm={4} alignContent="center" justify="center">
-                                        <Button color="inherit">
-                                            <AccountCircle/>
-                                            Entrar
-                                        </Button>
-                                    </Grid>
-                                    <Grid item xs={6} sm={4} alignContent="center" justify="center">
-                                        <Button color="inherit">
-                                            <ShoppingCart/>
-                                            Carrinho
-                                        </Button>
-                                    </Grid>
-                                    <Grid item xs={0} sm={1} alignContent="center" justify="center"/>
+                        <Grid item xs={12} sm={3} alignContent="center" justify="center">
+                            <Grid container spacing={8} alignItems="center">
+                                <Grid item xs={0} sm={3} alignContent="center" justify="center"/>
+                                <Grid item xs={6} sm={4} alignContent="center" justify="center">
+                                    <Button color="inherit">
+                                        <AccountCircle/>
+                                        Entrar
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={6} sm={4} alignContent="center" justify="center">
+                                    <Button color="inherit">
+                                        <ShoppingCart/>
+                                        Carrinho
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={0} sm={1} alignContent="center" justify="center"/>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Toolbar>
+                <Toolbar style={toolBarBottonHeader}>
+                    <Grid container spacing={0} alignItems="center" justify="center">
+                        <Grid item xs={12} alignContent="center" justify="center">
+                            <Grid container spacing={2} alignItems="center">
+                                <Grid item xs={1} alignContent="center" justify="center">
+                                    <div>
+                                        <IconButton className="menuButton" color="#212121" onClick={() => this.toggleDrawer()}>
+                                            <MenuIcon />
+                                        </IconButton>
+                                        
+                                    </div>
+                                </Grid>
+                                <Grid item xs={2} alignContent="center" justify="center">
+                                    <Button color='#212121'>
+                                        Promoções
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={2} alignContent="center" justify="center">
+                                    <Button color='#212121'>
+                                        Dia das mães
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={2} alignContent="center" justify="center">
+                                    <Button color='#212121'>
+                                        Dia dos Namorados
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={2} alignContent="center" justify="center">
+                                    <Button color='#212121'>
+                                        Copa do Mundo
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={2} alignContent="center" justify="center">
+                                    <Button color='#212121'>
+                                        Oferta do dia
+                                    </Button>
                                 </Grid>
                             </Grid>
                         </Grid>
-                    </Toolbar>
-                    <Toolbar style={toolBarBottonHeader}>
-                        <Grid container spacing={0} alignItems="center" justify="center">
-                            <Grid item xs={12} alignContent="center" justify="center">
-                                <Grid container spacing={2} alignItems="center">
-                                    <Grid item xs={1} alignContent="center" justify="center">
-                                        <div>
-                                            <IconButton className="menuButton" color="#212121" onClick={() => this.toggleDrawer()}>
-                                                <MenuIcon />
-                                            </IconButton>
-                                            <Drawer open={this.state.drawerOpened} onClose={() => this.toggleDrawer()}>
-                                                <List>
-                                                    <ListItem button>Eletrodomésticos</ListItem>
-                                                    <ListItem button>Smartphones</ListItem>
-                                                    <ListItem button>Informática</ListItem>
-                                                </List>
-                                                <Divider />
-                                                <List>
-                                                    <ListItem button>Souveniers</ListItem>
-                                                    <ListItem button>Livros</ListItem>
-                                                    <ListItem button>Beleza</ListItem>
-                                                </List>
-                                            </Drawer>
-                                        </div>
-                                    </Grid>
-                                    <Grid item xs={2} alignContent="center" justify="center">
-                                        <Button color='#212121'>
-                                            Promoções
-                                        </Button>
-                                    </Grid>
-                                    <Grid item xs={2} alignContent="center" justify="center">
-                                        <Button color='#212121'>
-                                            Dia das mães
-                                        </Button>
-                                    </Grid>
-                                    <Grid item xs={2} alignContent="center" justify="center">
-                                        <Button color='#212121'>
-                                            Dia dos Namorados
-                                        </Button>
-                                    </Grid>
-                                    <Grid item xs={2} alignContent="center" justify="center">
-                                        <Button color='#212121'>
-                                            Copa do Mundo
-                                        </Button>
-                                    </Grid>
-                                    <Grid item xs={2} alignContent="center" justify="center">
-                                        <Button color='#212121'>
-                                            Oferta do dia
-                                        </Button>
-                                    </Grid>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </Toolbar>
-                </AppBar>
-                
-            </div>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
         );
     }
 }
