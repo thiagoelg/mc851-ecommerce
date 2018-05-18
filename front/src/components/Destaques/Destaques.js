785217da583609b059fa691c1b7caf154a4a5b9c
Sabrina Beck Angelini <sabrinabeck.96@gmail.com>
2018-05-18 18:44:50 -0300

    add bind to categories on filter

--- front/src/components/Destaques/Destaques.js
+++ front/src/components/Destaques/Destaques.js
@@ -4,9 +4,27 @@ import Products from "../Products/Products";
 
 class Destaques extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: [],
+        }
+    };
+
+    componentDidMount() {
+        fetch('http://back.localhost/products?highlight=' + this.props.highlights)
+            .then(results => {
+                //TODO check if status ok
+                return results.json();
+            })
+            .then(data => {
+                this.setState({products: data});
+            })
+    }
+
     render() {
         return (
-            <Products highlights={true}/>
+            <Products products={this.state.products}/>
         );
     }
 
--- front/src/components/Products/Products.js
+++ front/src/components/Products/Products.js
@@ -4,41 +4,28 @@ import Subheader from 'material-ui/List/ListSubheader';
 import IconButton from 'material-ui/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
 
-var height = 280;
-var width = 280;
-
 class Products extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            products: [],
-        }
     };
 
-    componentDidMount() {
-        fetch('http://back.localhost/products?highlight=' + this.props.highlights)
-            .then(results => {
-                //TODO check if status ok
-                return results.json();
-            })
-            .then(data => {
-                this.setState({products: data});
-            })
-    }
-
     render() {
+        let cols = this.props.cols
+        if(cols === undefined)
+            cols = 4
+
         return (
             <div className="destaques" justify='center'>
-                <GridList cellHeight={height} cellWidth={width} cols={4}>
+                <GridList cellHeight={200} cols={cols}>
                     <GridListTile key="Subheader" cols={4} style={{height: 'auto'}}>
                         <Subheader component="div">Produtos em Destaque</Subheader>
                     </GridListTile>
-                    {this.state.products.map(tile => (
+                    {this.props.products.map(tile => (
                         <GridListTile key={tile.imageUrl} cols={1}>
                             <a href={"/products/" + tile.id}>
                                 <img src={tile.imageUrl} alt={tile.name}
-                                     style={{height: '100%', width: 'auto', margin: '0 auto', display: 'block'}}/>
+                                     style={{height: '100%', width: '100%', margin: '0 auto', display: 'block'}}/>
                                 <GridListTileBar
                                     title={tile.name}
                                     subtitle={<span>{tile.description} por {tile.price}</span>}
