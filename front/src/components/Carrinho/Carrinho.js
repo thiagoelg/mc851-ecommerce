ffc2d01b3e9c12d0ade7398a8e9e0bc2a1dcc7ce
Tamara Martinelli de Campos <tamaramcampos02@gmail.com>
2018-05-25 16:13:03 -0300

    alteracoes no carrinho

--- front/src/components/Carrinho/Carrinho.js
+++ front/src/components/Carrinho/Carrinho.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -7,6 +7,12 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { Grid, Button } from 'material-ui/es';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+
 
 const styles = theme => ({
   root: {
@@ -20,46 +26,102 @@ const styles = theme => ({
 });
 
 let id = 0;
-function createData(produto, qtd, preco) {
+function createData(imagem, produto, qtd, preco) {
   id += 1;
-  return { id, produto, qtd, preco };
+  return { id, imagem, produto, qtd, preco };
 }
 
+let imagePath = "lhama.jpg"
+
 const data = [
-  createData('Abajur Lhama', 1, 70.0),
-  createData('Abajur Esquilo', 1, 70.0),
-  createData('Abajur Baleia', 1, 70.0),
+  createData(imagePath, 'Abajur Lhama', 1, 70.0),
+  createData(imagePath, 'Abajur Esquilo', 1, 70.0),
+  createData(imagePath, 'Abajur Baleia', 1, 70.0),
 ];
 
-function SimpleTable(props) {
-  const { classes } = props;
+class SimpleTable extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+        product: {},
+        amount: 1
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-  return (
-    
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Produto</TableCell>
-            <TableCell numeric>Quantidade</TableCell>
-            <TableCell numeric>Preço</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map(n => {
-            return (
-              <TableRow key={n.id}>
-                <TableCell component="th" scope="row">
-                  {n.produto}
-                </TableCell>
-                <TableCell numeric>{n.qtd}</TableCell>
-                <TableCell numeric>{n.preco}</TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-   
-  );
+  handleChange(event) {
+    const target = event.target
+    this.setState({[target.name]: event.target.value})
+  }
+
+  render() {
+    const {classes} = this.props;
+
+    return (
+    <Grid container spacing={24}>
+      <Grid item xs={8}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Produto</TableCell>
+              <TableCell TextField>Quantidade</TableCell>
+              <TableCell></TableCell>
+              <TableCell numeric>Preço</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map(n => {
+              return (
+                <TableRow key={n.id}>
+                  <TableCell component="th" scope="row">
+                    <Grid container> 
+                      <Grid item xs={2}>
+                        <img src={n.imagePath} height='auto'/>
+                      </Grid>
+                      <Grid item xs={4}>
+                        {n.produto}
+                      </Grid>
+                    </Grid> 
+                  </TableCell>
+                  <TableCell>
+                    <TextField
+                      id="amount"
+                      value={this.state.amount}
+                      name="amount"
+                      onChange={this.handleChange}
+                      type="number"
+                      InputLabelProps={{
+                          shrink: true,
+                      }}
+                      margin="normal"
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <IconButton color="secondary" aria-label="Delete">
+                      <DeleteIcon />
+                    </IconButton>
+                  </TableCell>
+                  <TableCell numeric>{n.preco}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Grid>
+      <Grid item xs={4}>
+        <Paper>
+          <Typography variant="headline" component="h3">
+            This is a sheet of paper.
+          </Typography>
+          <Typography component="p">
+            Paper can be used to build surface or other elements for your application.
+          </Typography>
+        </Paper>
+      </Grid>
+    </Grid>
+    );
+  }
 }
 
 SimpleTable.propTypes = {
